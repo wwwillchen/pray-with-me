8e4f9177e37fafb160575e2e383e2fbee503a379
Will Chen <willchen90@gmail.com>
2017-11-21 20:14:43 -0800

    clean git

--- out/components/StyledText.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Text } from 'react-native';
-export class MonoText extends React.Component {
-    render() {
-        return React.createElement(Text, Object.assign({}, this.props, { style: [this.props.style, { fontFamily: 'space-mono' }] }));
-    }
-}
-//# sourceMappingURL=StyledText.js.map
